893f1dea2c8a9b8390615bd499599e0ad3f19f5e
Notteau Romain <romain_notteau@ens.univ-artois.fr>
2025-01-06 23:01:16 +0100

    ajout du CV en anglais

--- src/components/DownloadCVButton.js
+++ src/components/DownloadCVButton.js
@@ -2,13 +2,22 @@ import React from 'react';
 
 function DownloadCVButton() {
   return (
-    <a
-      href="/CV_Romain_Notteau_FR.pdf"
-      download
-      className="fixed top-4 left-4 z-50 bg-gray-700 text-white px-4 py-2 rounded-full hover:bg-gray-600 transition duration-300 shadow-lg"
-    >
-      Télécharger CV
-    </a>
+    <div className="fixed top-4 left-4 z-50 flex space-x-4">
+      <a
+        href="/CV_Romain_Notteau_FR.pdf"
+        download
+        className="bg-gray-700 text-white px-4 py-2 rounded-full hover:bg-gray-600 transition duration-300 shadow-lg"
+      >
+        Télécharger CV (FR)
+      </a>
+      <a
+        href="/CV_Romain_Notteau_EN.pdf"
+        download
+        className="bg-gray-700 text-white px-4 py-2 rounded-full hover:bg-gray-600 transition duration-300 shadow-lg"
+      >
+        Download CV (EN)
+      </a>
+    </div>
   );
 }
 
